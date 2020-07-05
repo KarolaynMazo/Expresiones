f863480f3b509e8548375037951e47c1deadf016
andreslaverde <andreslaverde11@gmail.com>
2020-07-04 23:26:12 -0500

    Se mejora la evaluación de expresiones
    Incluyendo las que tienen espacios y signos repetidos.

--- src/expresionesa/ExpresionesA.java
+++ src/expresionesa/ExpresionesA.java
@@ -19,16 +19,21 @@ public class ExpresionesA {
      */
     public static void main(String[] args) {
 
-        //String prefija = NotacionAritmetica.convertirInfijaAPrefija("3.7+5*2");
         String prefija = NotacionAritmetica.convertirInfijaAPrefija("3.7+5*2");
         System.out.println(prefija);
-       // System.out.println(NotacionAritmetica.evaluarPrefija("-+7*45+20"));
         System.out.println(NotacionAritmetica.evaluarPrefija(prefija));
         
         String prefija2 = NotacionAritmetica.convertirInfijaAPrefija("2^3+4*5.1");
         System.out.println(prefija2);
-       // System.out.println(NotacionAritmetica.evaluarPrefija("-+7*45+20"));
         System.out.println(NotacionAritmetica.evaluarPrefija(prefija2));
+        
+        String prefija4 = NotacionAritmetica.convertirInfijaAPrefija("-4 + ---3*   (2+ +5)");
+        System.out.println(prefija4);
+        System.out.println(NotacionAritmetica.evaluarPrefija(prefija4));
+        
+        String prefija5 = NotacionAritmetica.convertirInfijaAPrefija("(2 - -8 + 3 / 2)");
+        System.out.println(prefija5);
+        System.out.println(NotacionAritmetica.evaluarPrefija(prefija5));
     }
     
 }
--- src/expresionesa/NotacionAritmetica.java
+++ src/expresionesa/NotacionAritmetica.java
@@ -17,7 +17,79 @@ public class NotacionAritmetica {
     equivalente en notacion prefija
     */
     public static String convertirInfijaAPrefija(String expresionInfija){
-        Pila pilaInfija = obtenerPilaDeExpresion(expresionInfija);
+        Pila pilaInfija = obtenerPilaDeExpresion(expresionInfija, false);
+
+        Pila pilaPrefija = new Pila();
+        Pila pilaTemporal = new Pila();
+        String dato = "";
+        while(pilaInfija.estaVacia() == false){
+            dato = pilaInfija.desapilar().getDato();
+            
+            if(esDatoNumerico(dato)){
+                pilaPrefija.apilar(dato);
+            }else{
+                if(pilaTemporal.estaVacia()){
+                    pilaTemporal.apilar(dato);
+                    continue;
+                }
+                
+                int precedenciaDato = obtenerPrecedencia(dato.charAt(0));
+                int precedenciaTopePilaTemporal = obtenerPrecedencia(pilaTemporal.getTope().getDato().charAt(0));
+                
+                if(dato.equals(")") || dato.equals("(")){
+                    if(dato.equals(")")){
+                        pilaTemporal.apilar(dato);
+                    }else{
+                        boolean parar = false;
+                        
+                        while(pilaTemporal.estaVacia() == false && parar == false){
+                            if(pilaTemporal.getTope().getDato().equals(")")){
+                                pilaTemporal.desapilar();
+                                parar = true;
+                            }else{
+                                pilaPrefija.apilar(pilaTemporal.desapilar().getDato());
+                            }
+                        }
+                    }
+                }else{
+                    if(pilaTemporal.getTope().getDato().equals(")")){
+                        pilaTemporal.apilar(dato);
+                    }else{
+                        if(precedenciaDato >= precedenciaTopePilaTemporal){
+                            pilaTemporal.apilar(dato);
+                        }else{
+                            boolean parar = false;
+                            while(pilaTemporal.estaVacia() == false && parar == false){
+                                precedenciaTopePilaTemporal = obtenerPrecedencia(pilaTemporal.getTope().getDato().charAt(0));
+                                
+                                if(precedenciaTopePilaTemporal > precedenciaDato){
+                                    pilaPrefija.apilar(pilaTemporal.desapilar().getDato());
+                                    
+                                    if(pilaTemporal.estaVacia()){
+                                        pilaTemporal.apilar(dato);
+                                        parar = true;
+                                    }
+                                }else{
+                                    pilaTemporal.apilar(dato);
+                                    parar = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+        while(pilaTemporal.estaVacia() == false){
+            pilaPrefija.apilar(pilaTemporal.desapilar().getDato());
+        }
+        
+        return pilaPrefija.toString().replace(")", "");
+    }
+    
+    public static String convertirInfijaAPrefija_(String expresionInfija){
+        Pila pilaInfija = obtenerPilaDeExpresion(expresionInfija, false);
 
         Pila pilaPrefija = new Pila();
         Pila pilaTemporal = new Pila();
@@ -85,7 +157,7 @@ public class NotacionAritmetica {
     }
     
     public static double evaluarPrefija(String expresionPrefija){
-        Pila pilaPrefija = obtenerPilaDeExpresion(expresionPrefija);
+        Pila pilaPrefija = obtenerPilaDeExpresion(expresionPrefija, true);
         Pila pila = new Pila();
         
         while(pilaPrefija.estaVacia() == false){
@@ -114,74 +186,7 @@ public class NotacionAritmetica {
                     pila.apilar(String.valueOf(numero1 % numero2)); 
                     break; 
                 case '^': 
-                    pila.apilar(String.valueOf(Math.pow(numero2, numero2))); 
-                    break;
-                } 
-            }
-        }
-        
-        /*for (int i = expresionPrefija.length() - 1; i >= 0; i--) {
-            if(esCaracterNumero(expresionPrefija.charAt(i))){
-                pila.apilar(String.valueOf(expresionPrefija.charAt(i) - 48));
-            }else{
-                double numero1 = Double.parseDouble(pila.desapilar().getDato());
-                double numero2 = Double.parseDouble(pila.desapilar().getDato());
-                
-                switch (expresionPrefija.charAt(i)) { 
-                case '+': 
-                    pila.apilar(String.valueOf(numero1 + numero2)); 
-                    break; 
-                case '-': 
-                    pila.apilar(String.valueOf(numero1 - numero2)); 
-                    break; 
-                case '*': 
-                    pila.apilar(String.valueOf(numero1 * numero2)); 
-                    break; 
-                case '/': 
-                    pila.apilar(String.valueOf(numero1 / numero2)); 
-                    break; 
-                case '%': 
-                    pila.apilar(String.valueOf(numero1 % numero2)); 
-                    break; 
-                case '^': 
-                    pila.apilar(String.valueOf(Math.pow(numero2, numero2))); 
-                    break;
-                } 
-            }
-        }*/
-        
-        return Double.parseDouble(pila.getTope().getDato());
-    }
-    
-    public static double evaluarPrefija_(String expresionPrefija){
-        
-        Pila pila = new Pila();
-        
-        for (int i = expresionPrefija.length() - 1; i >= 0; i--) {
-            if(esCaracterNumero(expresionPrefija.charAt(i))){
-                pila.apilar(String.valueOf(expresionPrefija.charAt(i) - 48));
-            }else{
-                double numero1 = Double.parseDouble(pila.desapilar().getDato());
-                double numero2 = Double.parseDouble(pila.desapilar().getDato());
-                
-                switch (expresionPrefija.charAt(i)) { 
-                case '+': 
-                    pila.apilar(String.valueOf(numero1 + numero2)); 
-                    break; 
-                case '-': 
-                    pila.apilar(String.valueOf(numero1 - numero2)); 
-                    break; 
-                case '*': 
-                    pila.apilar(String.valueOf(numero1 * numero2)); 
-                    break; 
-                case '/': 
-                    pila.apilar(String.valueOf(numero1 / numero2)); 
-                    break; 
-                case '%': 
-                    pila.apilar(String.valueOf(numero1 % numero2)); 
-                    break; 
-                case '^': 
-                    pila.apilar(String.valueOf(Math.pow(numero2, numero2))); 
+                    pila.apilar(String.valueOf(Math.pow(numero1, numero2))); 
                     break;
                 } 
             }
@@ -190,13 +195,24 @@ public class NotacionAritmetica {
         return Double.parseDouble(pila.getTope().getDato());
     }
     
-    private static Pila obtenerPilaDeExpresion(String expresionInfija){
+    private static Pila obtenerPilaDeExpresion(String expresionInfija, boolean evaluarExpresion){
+        if(evaluarExpresion == false){
+            expresionInfija = ResolverOperadores(expresionInfija);
+        }
         Pila pilaInfija = new Pila();
         
         String dato = "";
         
         for (int i = 0; i < expresionInfija.length(); i++) {
             char digito = expresionInfija.charAt(i);
+            
+            if(i == 1){
+                if(esOperador(String.valueOf(digito)) && 
+                    expresionInfija.charAt(i) == expresionInfija.charAt(i-1)){
+                    dato = dato + expresionInfija.charAt(i);
+                    continue;
+                }
+            }
             
             if(esCaracterNumero(digito)){
                 
@@ -214,7 +230,12 @@ public class NotacionAritmetica {
                 }
             }
             
-            pilaInfija.apilar(String.valueOf(digito));
+            if(dato.equals("")){
+                pilaInfija.apilar(String.valueOf(digito));
+            }else{
+                pilaInfija.apilar(dato + digito);
+                dato = "";
+            }
         }
         
         if(dato.equals("") == false){
@@ -222,6 +243,59 @@ public class NotacionAritmetica {
         }
         
         return pilaInfija;
+    }
+    
+    private static String ResolverOperadores(String expresionInfija){
+        expresionInfija = expresionInfija.replace(" ", "");
+        boolean operadoresResueltos = false;
+        boolean operadoresAdyacentes = false;
+        StringBuilder sb = new StringBuilder();
+        
+        while(operadoresResueltos == false){
+            sb = new StringBuilder();
+            operadoresAdyacentes = false;
+            
+            for (int i = 0; i < expresionInfija.length(); i++) {
+                if(i < expresionInfija.length()- 1){
+                    String dato1 = String.valueOf(expresionInfija.charAt(i));
+                    String dato2 = String.valueOf(expresionInfija.charAt(i+1));
+                    
+                    if(esOperador(dato1) && esOperador(dato2)){
+                        operadoresAdyacentes = true;
+            
+                        if(dato1.equals("-") && dato2.equals("+")){
+                            sb.append("-");
+                        }
+
+                        if(dato1.equals("+") && dato2.equals("-")){
+                            sb.append("-");
+                        }
+
+                        if(dato1.equals("+") && dato2.equals("+")){
+                            sb.append("+");
+                        }
+
+                        if(dato1.equals("-") && dato2.equals("-")){
+                            sb.append("+");
+                        }
+
+                        i = i + 1;
+                    }else{
+                        sb.append(expresionInfija.charAt(i));
+                    }
+                }else{
+                    sb.append(expresionInfija.charAt(i));
+                }
+            }
+            
+            if(operadoresAdyacentes){
+                expresionInfija = sb.toString();
+            }else{
+                operadoresResueltos = true;
+            }
+        }
+        
+        return sb.toString();
     }
     
     private static Pila obtenerPilaDeExpresion_(String expresionInfija){
